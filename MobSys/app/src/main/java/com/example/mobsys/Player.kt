f408d66614332bfdbf0e41958407ce14faf58915
vadiminem <vadvc@yandex.ru>
2019-10-28 11:24:51 +0700

    fix

---
+++ MobSys/app/src/main/java/com/example/mobsys/Player.kt
@@ -0,0 +1,12 @@
+package com.example.mobsys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Player : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
